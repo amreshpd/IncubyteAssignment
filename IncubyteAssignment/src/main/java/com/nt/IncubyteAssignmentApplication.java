22e2609f0e1a3938613a86f15f92e1342cab3a4c
amreshpd <amreshpd98@gmail.com>
2024-11-11 11:26:22 +0530

    method calling through main method

--- IncubyteAssignment/src/main/java/com/nt/IncubyteAssignmentApplication.java
+++ IncubyteAssignment/src/main/java/com/nt/IncubyteAssignmentApplication.java
@@ -2,12 +2,17 @@ package com.nt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.nt.service.Addition;
 
 @SpringBootApplication
 public class IncubyteAssignmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IncubyteAssignmentApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(IncubyteAssignmentApplication.class, args);
+	Addition add = ctx.getBean(Addition.class);
+	add.add(10, 20);
 	}
 
 }
