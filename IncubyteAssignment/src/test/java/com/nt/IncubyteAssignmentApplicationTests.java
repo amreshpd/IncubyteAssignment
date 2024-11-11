3ac7b69d8cc6e682411cd0edba61975002197dca
amreshpd <amreshpd98@gmail.com>
2024-11-11 12:33:02 +0530

    test new line as separator

--- IncubyteAssignment/src/test/java/com/nt/IncubyteAssignmentApplicationTests.java
+++ IncubyteAssignment/src/test/java/com/nt/IncubyteAssignmentApplicationTests.java
@@ -13,37 +13,44 @@ import com.nt.service.Addition;
 class IncubyteAssignmentApplicationTests {
 
 	private static Addition addition;
-	
+
 	@BeforeAll
 	public static void stepOnce() {
-		addition=new Addition();
+		addition = new Addition();
 	}
-	
+
 	@Test
-    public void testEmptyString() throws Exception {
-        int actual=addition.add("");
-        int expected=0;
-        assertEquals(expected, actual);
-    }
-	
+	public void testEmptyString() throws Exception {
+		int actual = addition.add("");
+		int expected = 0;
+		assertEquals(expected, actual);
+	}
+
 	@Test
-    public void testSingleNumber() throws Exception {
-        int actual=addition.add("1");
-        int expected=1;
-        assertEquals(expected, actual);
-    }
+	public void testSingleNumber() throws Exception {
+		int actual = addition.add("1");
+		int expected = 1;
+		assertEquals(expected, actual);
+	}
+
 	@Test
-    public void testTwoNumbers() throws Exception {
-        assertEquals(9, addition.add("4,5"));
-    }
+	public void testTwoNumbers() throws Exception {
+		assertEquals(9, addition.add("4,5"));
+	}
+
 	@Test
-    public void testUnlimitedAmountOfNumbers() throws Exception {
-        assertEquals(17, addition.add("8,2,3,4"));
-    }
-	
+	public void testUnlimitedAmountOfNumbers() throws Exception {
+		assertEquals(17, addition.add("8,2,3,4"));
+	}
+
+	@Test
+	public void testNewlineAsSeparator() throws Exception {
+		assertEquals(11, addition.add("1\n2,3\n5"));
+	}
+
 	@AfterAll
 	public static void stepDown() {
-		addition=null;
+		addition = null;
 	}
-	
+
 }
