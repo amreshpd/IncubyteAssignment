398456da55a5710c8a4dd74f7c98ca9136d3ec2e
amreshpd <amreshpd98@gmail.com>
2024-11-11 12:24:44 +0530

    testing with empty number

--- IncubyteAssignment/src/test/java/com/nt/IncubyteAssignmentApplicationTests.java
+++ IncubyteAssignment/src/test/java/com/nt/IncubyteAssignmentApplicationTests.java
@@ -1,13 +1,28 @@
 package com.nt;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.nt.service.Addition;
 
 @SpringBootTest
 class IncubyteAssignmentApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private static Addition addition;
+	
+	@BeforeAll
+	public static void beforeTest() {
+		addition=new Addition();
 	}
-
+	
+	@Test
+    public void testEmptyString() throws Exception {
+        int actual=addition.add("");
+        int expected=0;
+        assertEquals(expected, actual);
+    }
+	
 }
